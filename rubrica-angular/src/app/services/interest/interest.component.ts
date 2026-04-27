3f17e3d43fe0e88ed6a7fbac10c4e92f183d4a7e
Igreg02 <I.gregale@gmail.com>
2026-04-27 11:01:43 +0200

    add a auth.services.ts

--- rubrica-angular/src/app/services/interest/interest.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-interest',
-  imports: [],
-  templateUrl: './interest.component.html',
-  styleUrl: './interest.component.css',
-})
-export class Interest {}
